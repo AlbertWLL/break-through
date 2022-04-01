d6b53875b678a016f711ef1ecab191eea1845c25
danque <danque@jushuitan.com>
2022-04-01 17:09:35 +0800

    自我突破自我成长

---
+++ src/main/java/com/example/danque/BreakThroughApplication.java
@@ -0,0 +1,13 @@
+package com.example.danque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreakThroughApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BreakThroughApplication.class, args);
+    }
+
+}
